868bae71b98b3dd38e462b2138b350a9bb4c4337
Mohammad Atef Al Kasawneh <eng.mohammad.khasawneh09@gmail.com>
2021-01-27 21:23:09 +0200

    2nd mod

---
+++ app.js
@@ -0,0 +1,16 @@
+alert('Hello')
+
+console.log(10 + 20);
+
+console.log('abc' + 'de');
+
+var UserName = 'sara';
+console.log(UserName);
+var pi = 3.14 + 10;
+var x = 5;
+var y = 5
+console.log(pi * x);
+console.log(pi>x);
+console.log(x >= pi)
+console.log(x >= y)
+
