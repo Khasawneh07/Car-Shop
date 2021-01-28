52e9da907a808f7b044ea90bf555a9b5662dabd7
Mohammad Atef Al Kasawneh <eng.mohammad.khasawneh09@gmail.com>
2021-01-28 21:04:57 +0200

    4thmod

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
-alert('Hello')
+var userinput = prompt ('Select Yor Color Black or blue you ');
+var x= "blue"
+var y= "black"
+var ss = " "
+console.log (x)
+console.log (y)
+console.log (ss)
+//
 
-console.log(10 + 20);
 
-console.log('abc' + 'de');
+while (userinput !== x && userinput !== y)
+{
+    userinput = prompt ('Enter Only Black Or blue')
+}
+    if (userinput == x)
+    {
+       ss = 'blue Cars';
+    } 
+    
+    else{if (userinput == y)
+  {
+    ss = 'Black Cars';
+  }
+  
+  }
+ 
+    document.write('<h2>' + ss + '</h2>');
 
-var UserName = 'sara';
-console.log(UserName);
-var pi = 3.14 + 10;
-var x = 5;
-var y = 5
-console.log(pi * x);
-console.log(pi>x);
-console.log(x >= pi)
-console.log(x >= y)
+var rating = prompt ('From 1 to 10 how passionate are you about owning this car?')
+var image=' ';
 
+for(var i = 0; i < rating; i++){
+    if(userinput =='blue'){
+        image = image + '<img src="https://s1.1zoom.me/prev/563/Nissan_2020_GT-R_50th_Anniversary_Gray_background_562832_600x400.jpg">';
+        console.log(image);
+        
+    }else if(userinput =='black'){
+        image = image +'<img src="https://tdrresearch.azureedge.net/photos/chrome/Expanded/White/2019NIC130001/2019NIC13000101.jpg">';
+  
+    }
+
+}
+
+document.write(image)
